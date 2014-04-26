446835082d3b64dc97e99858d4c8b4ef2038f504
Mak Ho Tin <smallcampustin@gmail.com>
2014-04-26 21:49:45 +0800

    import menudrawer

---
+++ SmartRecycleV2/src/com/example/smartrecyclev2/helper/SearchHelper.java
@@ -0,0 +1,14 @@
+package com.example.smartrecyclev2.helper;
+
+import com.example.smartrecyclev2.EventListener;
+
+public class SearchHelper {
+
+	public EventListener<String> onSearchComplete;
+	public EventListener onSearchFail;
+	
+	public String location;
+	public String date;
+	public String type;
+	
+}
